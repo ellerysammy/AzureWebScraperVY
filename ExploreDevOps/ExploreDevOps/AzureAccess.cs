128522cd58cac7f1a74450de0078c78fa0f66442
Ellery Valest <ellery.valest@col.vueling.com>
2020-04-02 13:52:11 +0200

    Initial commit

---
+++ ExploreDevOps/ExploreDevOps/AzureAccess.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.Services.Common;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreDevOps
+{
+    public class AzureAccess
+    {
+        public static async Task<List<object>> GetPipelines()
+        {
+            List<object> result = new List<object>();
+            string str_result = string.Empty;
+            string personalAccessToken = "lhci6kpvinehsgbnov7qmgnul4oz2lnbmc7htq4pq27sg6cgy2tq";
+            ////string url = "https://dev.azure.com/vueling";
+
+            string credentials = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", personalAccessToken)));
+
+            ////Uri uri = new Uri(url);
+            ////VssBasicCredential credentials = new VssBasicCredential("", personalAccessToken);
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri($"https://dev.azure.com/vueling"); 
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+
+                HttpResponseMessage response = client.GetAsync("/ams/_apis/build/definitions?stateFilter=All").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    str_result = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ExploreDevOps/ExploreDevOps/AzureWebScraper.cs
@@ -0,0 +1,49 @@
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExploreDevOps
+{
+    public class AzureWebScraper
+    {
+
+        public async Task<IHtmlDocument> GetHtmlDocFromAzure(string repoName, string project)
+        {
+            string personalAccessToken = "lhci6kpvinehsgbnov7qmgnul4oz2lnbmc7htq4pq27sg6cgy2tq";
+            string credentials = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", personalAccessToken)));
+            string siteUrl = $"https://dev.azure.com/vueling/{project}/_git/{repoName}/branchpolicies?refName=refs%2Fheads%2Fmaster";
+            CancellationTokenSource cancellationToken = new CancellationTokenSource();
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+            HttpResponseMessage request = await httpClient.GetAsync(siteUrl);
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            Stream response = await request.Content.ReadAsStreamAsync();
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            HtmlParser parser = new HtmlParser();
+            IHtmlDocument document = parser.ParseDocument(response);
+            return document;
+        }
+
+        public string GetJsonStringFromHtml(IHtmlDocument document)
+        {
+            string jsonResult = string.Empty;
+            var dataProvider = document.Scripts?.FirstOrDefault(x => x.Id == "dataProviders");
+            if (dataProvider != null)
+            {
+                jsonResult = dataProvider.InnerHtml;
+            }
+            return jsonResult;
+        }
+    }
+}
